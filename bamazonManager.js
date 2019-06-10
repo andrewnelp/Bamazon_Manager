3c9a34ed0df0c9afe031a203473f6fcab83cddd8
Andrey Nelepov <nelepow@gmail.com>
2019-06-10 07:30:36 -0400

    created boilerplate for js file

--- bamazonManager.js
+++ bamazonManager.js
@@ -18,5 +18,47 @@ connection.connect(err => {
   if (err) throw err;
   console.log(`connected as id ${connection.threadId}`);
   // run the start function after the connection is made to prompt the user
-  // displayProducts();
+  startSearch();
 });
+
+const startSearch = () => {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do, manager?",
+      choices: [
+        "View Products for Sale",
+        "View Low Inventory",
+        "Add to Inventory",
+        "Add New Product"
+      ]
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "View Products for Sale":
+          // viewProductsForSale();
+          break;
+
+        case "View Low Inventory":
+          // viewLowInv();
+          break;
+
+        case "Add to Inventory":
+          // addToInv();
+          break;
+
+        case "Add New Product":
+          // addNewProduct();
+          break;
+      }
+    });
+}
+
+//  * View Products for Sale
+
+  // * View Low Inventory
+
+  // * Add to Inventory
+
+  // * Add New Product
