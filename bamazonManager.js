290107c29525a4c43de9bfbfa4c86586134e85f8
Andrey Nelepov <nelepow@gmail.com>
2019-06-10 11:55:42 -0400

    low inventory is working

--- bamazonManager.js
+++ bamazonManager.js
@@ -37,11 +37,11 @@ const startSearch = () => {
     .then(function (answer) {
       switch (answer.action) {
         case "View Products for Sale":
-          // viewProductsForSale();
+          viewProductsForSale();
           break;
 
         case "View Low Inventory":
-          // viewLowInv();
+          viewLowInv();
           break;
 
         case "Add to Inventory":
@@ -57,7 +57,51 @@ const startSearch = () => {
 
 //  * View Products for Sale
 
+const viewProductsForSale = () => {
+  connection.query("SELECT * FROM products", (err, res) => {
+    if (err) throw err;
+    console.log('=============================');
+    console.log(`List of available products:`);
+    console.log('===============================================================================================');
+    res.forEach(r => {
+      console.log(`ID: ${r.item_id} || Name:${r.product_name} || Category: ${r.department_name} || Price: ${r.price} || Quantity ${r.stock_quantity} `);
+    })
+    console.log('===============================================================================================');
+    startSearch();
+  });
+  // startSearch();
+}
   // * View Low Inventory
+const viewLowInv = () =>{
+  inquirer
+    .prompt({
+      name: "lowinv",
+      type: "input",
+      message: "Type inventory number, please",
+      validate: value => {
+        if (isNaN(value) === false) {
+          return true;
+        }
+        return false;
+      }
+    })
+    .then(answer => {
+      let query = `SELECT item_id, product_name, department_name, price, stock_quantity `;
+      query += `FROM products WHERE item_id = ANY (SELECT item_id FROM products WHERE stock_quantity < ${answer.lowinv}) `;
+      query += `ORDER BY stock_quantity ASC`;
+      connection.query(query, function (err, res) {
+        if(err) throw err;
+        console.log('===============================================================================================');
+        res.forEach(r => {
+          console.log(`ID: ${r.item_id} || Name:${r.product_name} || Category: ${r.department_name} || Price: ${r.price} || Quantity ${r.stock_quantity} `)
+          console.log(`- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -`)
+        })
+        console.log('===============================================================================================');
+        startSearch();
+      });
+    });
+}
+
 
   // * Add to Inventory
 
